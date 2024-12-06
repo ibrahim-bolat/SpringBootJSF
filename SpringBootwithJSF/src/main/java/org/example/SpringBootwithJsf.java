4f1ac2affe5330d5e9d9835026352f36c04b191d
İbrahim Bolat <bolat6606@hotmail.com>
2024-12-06 14:19:46 +0300

    firstcommmit

---
+++ SpringBootwithJSF/src/main/java/org/example/SpringBootwithJsf.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import jakarta.faces.webapp.FacesServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringBootwithJsf {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootwithJsf.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean() {
+        FacesServlet servlet = new FacesServlet();
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(servlet, "*.jsf");
+        return servletRegistrationBean;
+    }
+
+
+}
---
+++ SpringBootwithJSF/src/main/java/org/example/bean/ProductController.java
@@ -0,0 +1,59 @@
+package org.example.bean;
+
+import jakarta.annotation.PostConstruct;
+import org.example.entity.Product;
+import org.example.service.ProductService;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Scope(value = "session")
+@Component
+public class ProductController {
+
+    private final ProductService productService;
+
+    private Product product;
+    private List<Product> products;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostConstruct
+    public void init() {
+        products = productService.getAllProducts();
+    }
+
+    public void saveProduct() {
+        productService.saveProduct(product);
+        products = productService.getAllProducts();
+        product = new Product();
+    }
+
+    public void deleteProduct(Long id) {
+        productService.deleteProduct(id);
+        products = productService.getAllProducts();
+    }
+
+    public void editProduct(Product product) {
+        this.product = product;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+}
